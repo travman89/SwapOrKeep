48297353b20a71466db348516d7585abdc32e336
travis davisson <travman89@csu.fullerton.edul>
2024-06-30 09:03:47 -0700

    Add lazy to image loading

--- src/Rules/index.tsx
+++ src/Rules/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Button } from "../components";
 import {
   FullScreen,
@@ -6,31 +7,44 @@ import {
   RulesText,
   ScrollImage,
 } from "./components";
-const Rules = ({ hideRules }: { hideRules: () => void }) => (
-  <FullScreen onClick={hideRules}>
-    <RulesContainer>
-      <ScrollImage src={"./scroll.webp"} />
-      <RulesHeading>♠ Swap or Keep rules ♠</RulesHeading>
-      <RulesText>
-        Each player is dealt a card. The player whose turn it is can choose to
-        keep their card or to swap it with the other player's card.
-        <br />
-        <br />
-        When the cards are revealed, the player with the highest card wins the
-        round and receives a point. In case of a tie, no points are awarded.
-        <br />
-        <br />
-        When the deck runs out, the player with the most points wins the game.
-        <br />
-        <br />
-        You can change the names of the players by clicking on them.
-        <br />
-        <br />
-        The game is pirate themed because pirates are cool.
-      </RulesText>
-      <Button onClick={hideRules}>Close</Button>
-    </RulesContainer>
-  </FullScreen>
-);
+const Rules = ({ hideRules }: { hideRules: () => void }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+  const loaded = () => {
+    setImageLoaded(true);
+  };
+  return (
+    <FullScreen onClick={hideRules}>
+      <RulesContainer>
+        <ScrollImage loading="lazy" src={"./scroll.webp"} onLoad={loaded} />
+        {imageLoaded && (
+          <>
+            <RulesHeading>♠ Swap or Keep rules ♠</RulesHeading>
+            <RulesText>
+              Each player is dealt a card. The player whose turn it is can
+              choose to keep their card or to swap it with the other player's
+              card.
+              <br />
+              <br />
+              When the cards are revealed, the player with the highest card wins
+              the round and receives a point. In case of a tie, no points are
+              awarded.
+              <br />
+              <br />
+              When the deck runs out, the player with the most points wins the
+              game.
+              <br />
+              <br />
+              You can change the names of the players by clicking on them.
+              <br />
+              <br />
+              The game is pirate themed because pirates are cool.
+            </RulesText>
+            <Button onClick={hideRules}>Close</Button>
+          </>
+        )}
+      </RulesContainer>
+    </FullScreen>
+  );
+};
 
 export { Rules };
